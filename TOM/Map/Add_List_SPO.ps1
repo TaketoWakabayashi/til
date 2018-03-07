73c51e6cd183d83452673ade45215a5269a0c59d
Taketo Wakabayashi <michaelowen628@gmail.com>
2018-03-07 17:22:39 +0900

    test

--- TOM/Map/Add_List_SPO.ps1
+++
@@ -1,8 +0,0 @@
-﻿Add-PSSnapin Microsoft.SharePoint.PowerShell
-#Adding the Client OM Assemblies        
-Add-Type -Path "Microsoft.SharePoint.Client.dll"
-Add-Type -Path "Microsoft.SharePoint.Client.Runtime.dll"
-
-$web = Get-SPWeb "https://pwcmsj.sharepoint.com/sites/TOM-TW/"
-$list = $web.Lists["BingMapList"]
-$list.Fields.AddFieldAsXml( "", $true, [Microsoft.SharePoint.SPAddFieldOptions]::AddFieldToDefaultView)
