eec38d41199d02c34f3af6d05bd93e0cbcf83648
Taketo Wakabayashi <taketo.wakabayashi@gmail.com>
2023-02-03 18:35:35 +0900

    ２章まで終了

---
+++ 202301_ruby-on-rails/toy_app/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+    root 'users#index'
+  end  
+end
